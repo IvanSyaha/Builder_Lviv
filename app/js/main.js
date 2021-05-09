cad7b9033f52b7c017716dd25b3c98af9c7d9d77
Ivan Hnyp <ivansyaha@gmail.com>
2021-05-09 23:07:09 +0300

    Added initial version

---
+++ app/js/main.js
@@ -0,0 +1,25 @@
+$(function () {
+    $('.portfolio__slider').slick({
+        dots: true,
+        responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                arrows: false,
+              },
+            },
+          ],
+    });
+
+    $('.reviews__slider').slick({
+        dots: true,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+            },
+          },
+        ],
+    });
+})
